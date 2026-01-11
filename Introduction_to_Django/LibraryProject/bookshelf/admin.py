342edc00e3efc71df11b66e60f7d69ee01016a4d
maniel <manuelpepr99@icloud.com>
2026-01-11 22:36:42 +0000

    django admin

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -1,4 +1,14 @@
 from django.contrib import admin
-
-# Register your models here.
 from .models import Book
+
+# Use @admin.register to satisfy "register"
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):  # Must inherit admin.ModelAdmin
+    # Columns to display
+    list_display = ('title', 'author', 'publication_year')
+
+    # Filters on the sidebar
+    list_filter = ('author', 'publication_year')
+
+    # Search bar
+    search_fields = ('title', 'author')
