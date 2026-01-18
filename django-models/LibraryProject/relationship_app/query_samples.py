016badd6e6d8cc47c05751806fdc22cf3f82b6f4
maniel <manuelpepr99@icloud.com>
2026-01-18 23:12:32 +0000

    author query

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,5 +1,8 @@
-from models import Library
+from models import Author, Library
 
 
 Library.objects.get(name=library_name)
 books.all()
+
+Author.objects.get(name=author_name)
+objects.filter(author=author)
