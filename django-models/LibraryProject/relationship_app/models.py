e43a79bf12f5352de187d9ccd231978e014e4747
maniel <manuelpepr99@icloud.com>
2026-01-18 22:54:40 +0000

    initial messsage

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -9,14 +9,23 @@ class Author(models.Model):
         return self.name
 
 
-class Book(models.model):
+class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.ForeignKey(Author)
 
-class Library(models.model):
+    def __str__(self):
+        return self.title
+
+class Library(models.Model):
     name = models.CharField(max_length=200)
     books = models.ManyToManyField(Book)
 
-class Librarian(models.model):
+    def __str__(self):
+        return self.namev
+
+class Librarian(models.Model):
     name = models.CharField(max_length=200)
     library = models.OneToOneField(Library)
+
+    def __str__(self):
+        return self.name
