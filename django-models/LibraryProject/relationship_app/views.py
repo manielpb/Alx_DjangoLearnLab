5c89de9ebc7e934e2c335ba4d18df7e07b3adc04
maniel <manuelpepr99@icloud.com>
2026-01-18 23:49:18 +0000

    userCreationForm

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from .models import Book
 from .models import Library
 from django.views.generic.detail import DetailView
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 def list_books(request):
