e7c402b362e0a48fe73ae58c08597d778ec2defd
maniel <manuelpepr99@icloud.com>
2026-01-19 00:56:59 +0000

    role based access

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,12 +1,17 @@
-from django.urls import path
+from django.urls import path, include
 from .views import list_books, LibraryDetailView
 from django.contrib.auth.views import LoginView, LogoutView
 from . import views
+from .views import admin_view, librarian_view, member_view
 
 urlpatterns = [
     path('books/', list_books, name='list_books'),
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
     path('register/', views.register, name='register'),
     path('login/',LoginView.as_view(template_name='relationship_app/'), name='login'),
-    path('logout/', LogoutView.as_view(template_name='relationship_app/'), name='logout')
+    path('logout/', LogoutView.as_view(template_name='relationship_app/'), name='logout'),
+    path('admin/', admin_view, name='admin_view'),
+    path('librarian/', librarian_view, name="librarian_view"),
+    path('member/', member_view, name='member_view'),
+    path('roles/', include('relationship_app.urls')),
 ]   
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -7,6 +7,8 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
 from django.http import HttpResponse, HttpResponseForbidden
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import user_passes_test
+
 
 
 # Create your views here.
@@ -29,8 +31,29 @@ def register(request):
         form = UserCreationForm()
     return render(request, 'relationship_app/register.html', {'form': form})
 
-@login_required
-def admin_view(request):
+#@login_required
+#def admin_view(request):
     if request.user.userprofile.role != 'Admin':
         return HttpResponseForbidden("You are not authorized to view this page.")
     return HttpResponse("Welcome Admin! You have full access.")
+
+def is_admin(user):
+    return user.is_authenticated and user.userprofile.role == 'Admin'
+
+def is_librarian(user):
+    return user.is_authenticated and user.userprofile.role == 'Librarian'
+
+def is_member(user):
+    return user.is_authenticated and user.userprofile.role == 'Member'
+
+@user_passes_test(is_admin)
+def admin_view(request):
+    return HttpResponse("Admin Dashboard")
+
+@user_passes_test(is_librarian):
+def librarian_view(request):
+    return HttpResponse("Librarian Dashboard")
+
+@user_passes_test(is_member)
+def member_view(request):
+    return HttpResponse("Member Dashboard")
