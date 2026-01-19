d3355761e9438e8e6b7597bdefb1d32416bbef5f
maniel <manuelpepr99@icloud.com>
2026-01-19 01:19:51 +0000

    role based access

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -35,9 +35,7 @@ class Librarian(models.Model):
 
     
 class UserProfile(models.Model):
-    
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-
     role = models.CharField(
         max_length=20,
         choices=[
@@ -46,6 +44,3 @@ class UserProfile(models.Model):
             ("Member", "Member"),
         ]
     )
-
-    def __str__(self):
-        return f"{self.user.username} - {self.role}"
