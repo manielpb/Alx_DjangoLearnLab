e142998973d75de7174e3aeb7d955b7c4da0134d
maniel <manuelpepr99@icloud.com>
2026-01-25 17:38:46 +0000

    CustomUserManager

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Book
+from .models import Book, CustomUser
+from django.contrib.auth.admin import UserAdmin
 
 # Use @admin.register to satisfy "register"
 @admin.register(Book)
@@ -12,3 +13,23 @@ class BookAdmin(admin.ModelAdmin):  # Must inherit admin.ModelAdmin
 
     # Search bar
     search_fields = ('title', 'author')
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    list_display = ("username", "email", "date_of_birth", "is_staff", "is_active")
+
+    list_filter = ("is_staff", "is_superuser", "is_active", "groups")
+
+    search_fields = ("username", "email")
+
+    ordering = ("username",)
+
+    fieldsets = UserAdmin.fieldsets + (
+        ("Profile Info", {"fields": ("date_of_birth", "profile_photo")}),
+    )
+
+    add_fieldsets = UserAdmin.add_fieldsets + (
+    (
+        "Profile Info", {"fields": ("date_of_birth", "profile_photo")}
+    ),
+    )
