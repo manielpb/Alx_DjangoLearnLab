709afe7748614eeb57610f5ca4ae708d5cffa19c
maniel <manuelpepr99@icloud.com>
2026-01-25 16:55:22 +0000

    initial commit

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser,BaseUserManager
 
 # Create your models here.
 class Book(models.Model):
@@ -11,3 +11,10 @@ class Book(models.Model):
 class CustomUser(AbstractUser):
     date_of_birth = models.DateField()
     profile_photo = models.ImageField()
+
+class CustomUserManager(BaseUserManager):
+    def create_user():
+        pass
+
+    def create_superuser():
+        pass
