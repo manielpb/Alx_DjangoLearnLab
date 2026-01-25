f60aec59a50d14756853db4c2b6333e61b0a30c9
maniel <manuelpepr99@icloud.com>
2026-01-25 17:53:09 +0000

    AUTH_USER_MODEL

--- advanced_features_and_security/LibraryProject/LibraryProject/settings.py
+++ advanced_features_and_security/LibraryProject/LibraryProject/settings.py
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
     'bookshelf',
 ]
 
+AUTH_USER_MODEL = "bookshelf.CustomUser"
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
