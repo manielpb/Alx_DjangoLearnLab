6977b2c8b4748d327b6a4d2c8e54b6cf6ab3797e
maniel <manuelpepr99@icloud.com>
2026-01-25 21:51:57 +0000

    security practices

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import permission_required
 from .models import Book
-from .forms import BookSearchForm
+from .forms import BookSearchForm, ExampleForm
 from django.db.models import Q
 
 # Create your views here.
