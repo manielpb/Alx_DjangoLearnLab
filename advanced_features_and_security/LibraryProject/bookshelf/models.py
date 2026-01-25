988b8a24e6fee3d5c5ad1e98eecde87d2e3f466e
maniel <manuelpepr99@icloud.com>
2026-01-25 17:17:13 +0000

    custom user model and manager

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -5,16 +5,48 @@ from django.contrib.auth.models import AbstractUser,BaseUserManager
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
-    published_year = models.IntegerField
+    published_year = models.IntegerField()
+
+
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, username, password=None, date_of_birth=None, profile_photo=None, **extra_fields):
+        if not username:
+            raise ValueError("Users must have a username")
+        
+        user = self.model(
+            username=username,
+            date_of_birth=date_of_birth,
+            profile_photo=profile_photo,
+            **extra_fields
+        )
+        user.set_password(password)
+        user.save(using=self.db)
+        return user
+        
+
+    def create_superuser(self, username, password=None, date_of_birth=None, profile_photo=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("is_active", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+
+        return self.create_user(
+            username=username,
+            password=password,
+            date_of_birth=date_of_birth,
+            profile_photo=profile_photo,
+            **extra_fields        
+            )
+    
 
 
 class CustomUser(AbstractUser):
-    date_of_birth = models.DateField()
-    profile_photo = models.ImageField()
+    date_of_birth = models.DateField(null=True, blank=True)
+    profile_photo = models.ImageField(upload_to="profile+photos/", null=True, blank=True)
 
-class CustomUserManager(BaseUserManager):
-    def create_user():
-        pass
-
-    def create_superuser():
-        pass
+    objects = CustomUserManager()
