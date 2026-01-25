fda70ec2e1f14190c2ba6b5ba595607052b34929
maniel <manuelpepr99@icloud.com>
2026-01-25 18:52:42 +0000

    Initial commit

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -7,6 +7,17 @@ class Book(models.Model):
     author = models.CharField(max_length=100)
     published_year = models.IntegerField()
 
+    class Meta:
+        permissions = (
+            ("can_view", "Can view book"),
+            ("can_create", "Can create book"),
+            ("can_edit", "Can edit book"),
+            ("can_delete", "Can delete book")
+        )
+
+        def __str__(self):
+            return self.title
+
 
 
 class CustomUserManager(BaseUserManager):
