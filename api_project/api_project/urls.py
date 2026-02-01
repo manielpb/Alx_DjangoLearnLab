eadd4ee32048f256426be12b37a3fe70695ed50e
maniel <manuelpepr99@icloud.com>
2026-02-01 21:25:55 +0000

    Permissions

--- api_project/api_project/urls.py
+++ api_project/api_project/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("api/", include('api.urls'))
+    path("api/", include('api.urls')),
+    path("api/token/", obtain_auth_token, name="api_token_auth"),
 ]
