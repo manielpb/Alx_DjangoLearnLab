0780989b1443896b3c28cad0b90a70e87edb8ecd
maniel <manuelpepr99@icloud.com>
2026-02-08 03:08:26 +0000

    Permissions and filtering

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -5,6 +5,7 @@ from .serializers import BookSerializer
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from rest_framework.filters import SearchFilter, OrderingFilter
 from django_filters.rest_framework import DjangoFilterBackend
+from django_filters import rest_framework
 
 # Create your views here.
 class ListView(generics.ListAPIView):
