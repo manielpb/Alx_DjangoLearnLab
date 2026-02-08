44486302a987c897a654736bd8b766f4651ddc32
maniel <manuelpepr99@icloud.com>
2026-02-08 02:42:53 +0000

    Generic views

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -10,7 +10,7 @@ from .views import (
 urlpatterns = [
     path("books/<int:pk>/", ListView.as_view(), name="book-list"),
     path("books/<int:pk>/", DetailView.as_view(), name="book-detail"),
-    path("books/<int:pk>/create/", CreateView.as_view(), name="books-create"),
-    path("books/<int:pk>/update", UpdateView.as_view(), name="book-update"),
-    path("books/<int:pk>/delete", DeleteView.as_view(), name = "book-delete")
+    path("books/create/", CreateView.as_view(), name="books-create"),
+    path("books/update", UpdateView.as_view(), name="book-update"),
+    path("books/delete", DeleteView.as_view(), name = "book-delete")
 ]
