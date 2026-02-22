8ef14bd059132ae129f81486b1a4bc7cab43c6a0
maniel <manuelpepr99@icloud.com>
2026-02-22 16:58:31 +0000

    notifications and likes functionality

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -9,6 +9,6 @@ router.register(r"comments", CommentViewSet, basename="comments")
 urlpatterns = [
     path("", include(router.urls)),
     path("feed/", FeedView.as_view(), name="feed"),
-    path("posts/<int:post_id>/like/", LikePostView.as_view(), name="like_post"),
-    path("posts/<int:post_id>/unlike/", UnlikePostView.as_view(), name="unlike_post"),
+    path("posts/<int:pk>/like/", LikePostView.as_view(), name="like_post"),
+    path("posts/<int:pk>/unlike/", UnlikePostView.as_view(), name="unlike_post"),_post"),
 ]
