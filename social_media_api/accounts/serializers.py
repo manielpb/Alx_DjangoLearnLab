a99deab69d728ea0f3ce3759b6f25905c521c3f8
maniel <manuelpepr99@icloud.com>
2026-02-22 15:03:42 +0000

    initial commit

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import get_user_model, authenticate
 from rest_framework import serializers
+from rest_framework.authtoken.models import Token
 
 User = get_user_model()
 
@@ -11,30 +12,37 @@ class RegisterSerializer(serializers.ModelSerializer):
         fields = ["username", "email", "password", "bio", "profile_picture"]
 
     def create(self, validated_data):
-        password = validated_data.pop("password")
-        user = User(**validated_data)
-        user.set_password(password)
-        user.save()
-        return user
+      user = get_user_model().objects.create_superuser(
+          username=validated_data["username"],
+          email=validated_data.get("email"),
+          password=validated_data["password"],
+          bio=validated_data.get("bio", ""),
+          profile_picture=validated_data.get("profile_picture", None),
+      )
+
+      Token.objects.create(user=user)
+
+      return user
     
 
 
 class LoginSerializer(serializers.Serializer):
     username = serializers.CharField()
-    password = serializers.CharField(write_only= True)
+    password = serializers.CharField(write_only=True)
 
-    def validate(self, attrs):
+    def validate(self, data):
         user = authenticate(
-            username = attrs.get("username"),
-            passowrd = attrs.get("password")
+            username=data["username"],
+            password=data["password"]
         )
-        if not user:
-            raise serializers.ValidationError("Invalid credentials.")
-        attrs["user"] = user
-        return attrs
-    
 
-    class UserSerializer(serializers.ModelSerializer):
+        if not user:
+            raise serializers.ValidationError("Invalid credentials")
+
+        data["user"] = user
+        return data
+    
+class UserSerializer(serializers.ModelSerializer):
         class Meta:
             model = User
             fields = ["id", "username", "email", "bio", "profile_picture"]
