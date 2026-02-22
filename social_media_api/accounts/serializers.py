62e2ade3aa165414adf2034cc3d6d59cb96d4dcf
maniel <manuelpepr99@icloud.com>
2026-02-22 15:07:47 +0000

    initial commit

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -12,17 +12,17 @@ class RegisterSerializer(serializers.ModelSerializer):
         fields = ["username", "email", "password", "bio", "profile_picture"]
 
     def create(self, validated_data):
-      user = get_user_model().objects.create_superuser(
-          username=validated_data["username"],
-          email=validated_data.get("email"),
-          password=validated_data["password"],
-          bio=validated_data.get("bio", ""),
-          profile_picture=validated_data.get("profile_picture", None),
-      )
+        user = get_user_model().objects.create_user(
+        username=validated_data["username"],
+        email=validated_data.get("email"),
+        password=validated_data["password"],
+        bio=validated_data.get("bio", ""),
+        profile_picture=validated_data.get("profile_picture", None),
+        )
 
-      Token.objects.create(user=user)
+        Token.objects.create(user=user)
 
-      return user
+        return user
     
 
 
