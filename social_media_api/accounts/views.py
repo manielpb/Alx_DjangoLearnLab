e7e17d864da1410a50510b61af39d4aa11d55253
maniel <manuelpepr99@icloud.com>
2026-02-22 15:21:58 +0000

    database setup

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework.authtoken
+from rest_framework.authtoken.models import Token
 from .serializers import RegisterSerializer, UserSerializer, LoginSerializer
 
 # Create your views here.
