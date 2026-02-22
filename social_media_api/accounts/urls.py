ea581f4572151df26c4dca60fb903ffec0780ba4
maniel <manuelpepr99@icloud.com>
2026-02-22 15:42:09 +0000

    user follows and feed functionaity

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -1,7 +1,12 @@
-from django.urls import path
-from .views import RegisterView, LoginView
+from django.urls import path, include
+from .views import RegisterView, LoginView, UserViewSet
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register(r"users", UserViewSet, basename="users")
 
 urlpatterns = [
     path("register/", RegisterView.as_view(), name="register"),
     path("login/", LoginView.as_view(), name="login"),
+    path("", include(router.urls)),
 ]
--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
-
 from .models import Post, Comment
 from .serializers import PostSerializer, CommentSerializer
 from .permissions import IsOwnerOrReadOnly
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
 class PostViewSet(viewsets.ModelViewSet):
@@ -24,3 +25,17 @@ class CommentViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
+
+class FeedView(generics.ListAPIView):
+    serializer_class = PostSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        following_ids = user.following.values_list("id", flat=True)
+
+        return (
+            Post.objects
+            .filter(author_id__in=following_ids)
+            .order_by("-created_at")
+        )
